53d8c4aea1dc09d3f3c5eca4ac543871f8b9af4b
mannycolon <colonmanuel7@gmail.com>
2021-02-02 13:43:32 -0500

    Reverted

--- src/ProskommaContext/index.js
+++ src/ProskommaContext/index.js
@@ -1,10 +1,13 @@
 import React, { createContext, useState, useContext } from 'react';
 import { ProsKomma } from 'proskomma';
 
+const pk = new ProsKomma();
+export const ProskommaStateContext = createContext(pk);
+
 export const ProskommaContext = createContext({});
 
 export default function ProsKommaContextProvider(props) {
-  const pk = new ProsKomma();
+  const pk = useContext(ProskommaStateContext);
   const [pkChangeId, setPkChangeId] = useState(0);
 
   const value = {
