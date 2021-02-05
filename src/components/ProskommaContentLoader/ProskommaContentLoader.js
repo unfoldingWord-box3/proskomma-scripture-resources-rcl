d2212e7702c7995f0a02758d20a1f81b3a8ef8d8
Mark Howe <mvahowe@gmail.com>
2021-02-05 17:34:09 +0100

    ProskommaContentLoader imports document from USFM string

--- src/components/ProskommaContentLoader/ProskommaContentLoader.js
+++ src/components/ProskommaContentLoader/ProskommaContentLoader.js
@@ -5,31 +5,45 @@ import {ProskommaContext} from '../../ProskommaContext';
 // import { getDCSDocuments } from '../../LoadUtils/download';
 
 const ProskommaContentLoader = ({children}) => {
-    const {pk} = useContext(ProskommaContext);
+    const {pk, pkChangeId, setPkChangeId} = useContext(ProskommaContext);
     const [processingState, setProcessingState] = useState("Idle");
 
     useEffect(
         () => {
-            React.Children.toArray(children)
-                .forEach(
-                    // async
-                    c => {
-                        switch (c.props.type) {
-                            case "DCSVolume":
-                                console.log("DCS Volume");
-                                /* await getDCSDocuments(
-                                    pk,
-                                    c.props.url,
-                                    {org: c.props.org, lang: c.props.lang, abbr: c.props.lang}
-                                    );
+            if (pk.nDocuments() === 0) {
+                React.Children.toArray(children)
+                    .forEach(
+                        // async
+                        c => {
+                            switch (c.props.type) {
+                                case "DCSVolume":
+                                    console.log("DCS Volume");
+                                    /* await getDCSDocuments(
+                                        pk,
+                                        c.props.url,
+                                        {org: c.props.org, lang: c.props.lang, abbr: c.props.lang}
+                                        );
 
-                                 */
-                                break;
-                            default:
-                                console.log(`Unknown LoadSpec Type '${c.props.type}'`)
+                                     */
+                                    break;
+                                case "usfmString":
+                                    try {
+                                        pk.importDocument(
+                                            {org: c.props.org, lang: c.props.lang, abbr: c.props.lang},
+                                            'usfm',
+                                            c.props.usfm
+                                        );
+                                        setPkChangeId(pkChangeId + 1);
+                                    } catch (err) {
+                                        console.log(`Could not load document: ${err}`);
+                                    }
+                                    break;
+                                default:
+                                    console.log(`Unknown LoadSpec Type '${c.props.type}'`)
+                            }
                         }
-                    }
-                );
+                    );
+            }
         },
         []
     );
--- src/components/ProskommaContentLoader/ProskommaContentLoader.md
+++ src/components/ProskommaContentLoader/ProskommaContentLoader.md
@@ -17,6 +17,20 @@ import {UWProskomma} from 'uw-proskomma';
             abbr={"uhb"}
             url={"https://git.door43.org/unfoldingWord/hbo_uhb/raw/branch/master"}
         />
+        <LoadSpec
+            type={"usfmString"}
+            org={"unfoldingWord"}
+            lang={"en"}
+            abbr={"ust"}
+            usfm={'\\id MRK Mark\'s Gospel, translated by Mark\n' +
+            '\\toc1 The Gospel of Mark\n' +
+            '\\toc2 Mark\n' +
+            '\\toc3 Mk\n' +
+            '\\c 1\n' +
+            '\\v 1\n' +
+            '\\p\n' +
+            'This is how the Good News of JC began...\n'}
+        />
     </ProskommaContentLoader>
 </ProsKommaContextProvider>;
 ```
